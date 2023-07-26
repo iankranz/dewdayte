82a113d5d3b7964caace58f8e917291f3ee3ec24
iankranz <ian@iankranz.com>
2023-07-26 19:07:28 -0400

    break out room flows into pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import { api } from "@/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const room = api.room.create.useMutation();
-
-  function createRoom() {
-    room.mutate({ name: "test room" });
-  }
-
   return (
     <>
       <Head>
@@ -22,42 +14,12 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             <span className="text-[hsl(280,100%,70%)]">dewdayte</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-          <input />
-          <input />
-          <button className="text-white">create todo</button>
-          <button className="text-white" onClick={createRoom}>
-            create room
-          </button>
-          <p className="text-2xl text-white">
-            {room.data ? room.data.id : "Loading tRPC query..."}
-          </p>
+          <Link href="/room/create">
+            <button className="text-white">create new room</button>
+          </Link>
+          <Link href="/room/join">
+            <button className="text-white">join existing room</button>
+          </Link>
         </div>
       </main>
     </>
---
+++ src/pages/room/create.tsx
@@ -0,0 +1,41 @@
+import { api } from "@/utils/api";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function CreateRoomPage() {
+  const room = api.room.create.useMutation();
+
+  const [name, setName] = useState<string | null>(null);
+
+  function createRoom() {
+    if (!name) return;
+    room.mutate({ name });
+  }
+
+  function handleNameInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const target = e.target as HTMLInputElement;
+    setName(target.value);
+  }
+
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            <Link href="/">
+              <span className="text-[hsl(280,100%,70%)]">dewdayte</span>
+            </Link>
+          </h1>
+          <p className="text-white">enter a name for the room</p>
+          <input onChange={handleNameInput} />
+          <button className="text-white" onClick={createRoom}>
+            create
+          </button>
+          <p className="text-2xl text-white">
+            {room.data ? room.data.id : "no room id"}
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
